ec85472f9ac743e4420cbd506c9aa067fca03b9d
SireniaPbot <erke12@hotmai.com>
2013-05-12 16:00:35 +0300

    Update ResetNode.java

--- nodes/ResetNode.java
+++ nodes/ResetNode.java
@@ -3,17 +3,13 @@ package org.sirenia.scripts.qbutterflies.nodes;
 import org.powerbot.core.script.job.state.Node;
 import org.powerbot.game.api.methods.Walking;
 import org.powerbot.game.api.methods.interactive.Players;
-import org.powerbot.game.api.methods.interactive.NPCs;
-import org.powerbot.game.api.wrappers.interactive.NPC;
-
 import org.sirenia.scripts.qbutterflies.misc.Vars;
 
 public class ResetNode extends Node {
 
 	@Override
 	public boolean activate() {
-		NPC butterfly = NPCs.getNearest(Vars.butterfly);
-		return Vars.reset_tile != null && butterfly == null || !Vars.hunting_area.contains(Players.getLocal().getLocation());
+		return Vars.reset_tile != null && Vars.butterflai == null || !Vars.hunting_area.contains(Players.getLocal().getLocation());
 	}
 
 	@Override
