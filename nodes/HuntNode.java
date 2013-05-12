663a030ac93cebf9db978d3a401df307f48bfa17
SireniaPbot <erke12@hotmai.com>
2013-05-12 14:58:55 +0200

    Update HuntNode.java

--- nodes/HuntNode.java
+++ nodes/HuntNode.java
@@ -2,28 +2,27 @@ package org.sirenia.scripts.qbutterflies.nodes;
 
 import org.powerbot.core.script.job.state.Node;
 import org.powerbot.game.api.methods.Walking;
-import org.powerbot.game.api.methods.interactive.NPCs;
 import org.powerbot.game.api.methods.interactive.Players;
-import org.powerbot.game.api.wrappers.interactive.NPC;
-
+import org.powerbot.game.api.util.Timer;
 import org.sirenia.scripts.qbutterflies.misc.Vars;
 
 public class HuntNode extends Node {
 
 	@Override
 	public boolean activate() {
-		NPC butterfly = NPCs.getNearest(Vars.butterfly);
-		return !Vars.needToSummon && Vars.hunting_area.contains(Players.getLocal().getLocation()) && Players.getLocal().getInteracting() == null && Players.getLocal().isIdle() && butterfly != null;
+		return !Vars.needToSummon && Vars.hunting_area.contains(Players.getLocal().getLocation()) && Players.getLocal().getInteracting() == null && Players.getLocal().isIdle() && Vars.butterflai != null;
 	}
 
 	@Override
 	public void execute() {
 		Vars.status = "HuntingNode";
-		NPC butterfly = NPCs.getNearest(Vars.butterfly);
-		if (!butterfly.isOnScreen()) {
-			Walking.walk(butterfly);
-		} else if (butterfly.validate() && butterfly.interact("Catch")) {
-			sleep(500, 800);
+		if (!Vars.butterflai.isOnScreen()) {
+			Walking.walk(Vars.butterflai);
+		} else if (Vars.butterflai.validate() && Vars.butterflai.interact("Catch")) {
+			Timer t = new Timer(1000);
+			while (t.isRunning() && Players.getLocal().getInteracting() == null) {
+				sleep(200, 300);
+			}
 		}
 	}
 }
